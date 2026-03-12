9caca84242fb163441550cf54f14bcda24a44e3d
GuilhermeSouzaSafesoft <guilherme.souza@safesoft.com.br>
2026-03-11 22:16:52 -0300

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,8 @@
 
+from pathlib import Path
+
+BASE_DIR = Path(__file__).parent
+TEMPLATE_FILE = BASE_DIR / "template.docx"
+JSON_FILE = BASE_DIR / "dados_json.json"
+OUTPUT_DIR = BASE_DIR / "output"
+OUTPUT_FILE = OUTPUT_DIR / "documento_final.docx"
