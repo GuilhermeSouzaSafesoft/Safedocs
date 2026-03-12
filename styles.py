4e37e77eacbf6b6b5e847bdb7dc727eed9acffd8
GuilhermeSouzaSafesoft <guilherme.souza@safesoft.com.br>
2026-03-11 21:57:33 -0300

    Update styles.py

--- styles.py
+++ styles.py
@@ -1,1 +1,171 @@
+BLUE_HEX = "0123A1"
+WHITE_HEX = "FFFFFF"
+BLACK_HEX = "000000"
 
+
+PARAGRAPH_STYLES = {
+    "title": {
+        "align": "center",
+        "space_before": 0,
+        "space_after": 12,
+    },
+    "heading_1": {
+        "align": "left",
+        "space_before": 12,
+        "space_after": 6,
+    },
+    "heading_2": {
+        "align": "left",
+        "space_before": 10,
+        "space_after": 4,
+    },
+    "heading_3": {
+        "align": "left",
+        "space_before": 8,
+        "space_after": 3,
+    },
+    "heading_4": {
+        "align": "left",
+        "space_before": 6,
+        "space_after": 3,
+    },
+    "body": {
+        "align": "justify",
+        "space_before": 0,
+        "space_after": 6,
+    },
+    "bullet": {
+        "align": "left",
+        "space_before": 0,
+        "space_after": 2,
+    },
+    "number": {
+        "align": "left",
+        "space_before": 0,
+        "space_after": 2,
+    },
+    "caption": {
+        "align": "center",
+        "space_before": 3,
+        "space_after": 8,
+    },
+    "table_header": {
+        "align": "center",
+        "space_before": 0,
+        "space_after": 0,
+    },
+    "table_cell": {
+        "align": "left",
+        "space_before": 0,
+        "space_after": 0,
+    },
+}
+
+
+RUN_STYLES = {
+    "title": {
+        "font_name": "Corbel",
+        "font_size": 28,
+        "bold": True,
+        "italic": False,
+        "color": BLUE_HEX,
+    },
+    "heading_1": {
+        "font_name": "Corbel",
+        "font_size": 20,
+        "bold": True,
+        "italic": False,
+        "color": BLUE_HEX,
+    },
+    "heading_2": {
+        "font_name": "Corbel",
+        "font_size": 16,
+        "bold": True,
+        "italic": False,
+        "color": BLUE_HEX,
+    },
+    "heading_3": {
+        "font_name": "Corbel",
+        "font_size": 14,
+        "bold": True,
+        "italic": False,
+        "color": BLUE_HEX,
+    },
+    "heading_4": {
+        "font_name": "Corbel",
+        "font_size": 12,
+        "bold": True,
+        "italic": False,
+        "color": BLUE_HEX,
+    },
+    "body": {
+        "font_name": "Corbel",
+        "font_size": 11,
+        "bold": False,
+        "italic": False,
+        "color": BLACK_HEX,
+    },
+    "bullet": {
+        "font_name": "Corbel",
+        "font_size": 11,
+        "bold": False,
+        "italic": False,
+        "color": BLACK_HEX,
+    },
+    "number": {
+        "font_name": "Corbel",
+        "font_size": 11,
+        "bold": False,
+        "italic": False,
+        "color": BLACK_HEX,
+    },
+    "caption": {
+        "font_name": "Corbel",
+        "font_size": 10,
+        "bold": False,
+        "italic": True,
+        "color": BLACK_HEX,
+    },
+    "table_header": {
+        "font_name": "Corbel",
+        "font_size": 11,
+        "bold": True,
+        "italic": False,
+        "color": WHITE_HEX,
+    },
+    "table_cell": {
+        "font_name": "Corbel",
+        "font_size": 11,
+        "bold": False,
+        "italic": False,
+        "color": BLACK_HEX,
+    },
+    "bold": {
+        "font_name": "Corbel",
+        "font_size": 11,
+        "bold": True,
+        "italic": False,
+        "color": BLUE_HEX,
+    },
+    "blue": {
+        "font_name": "Corbel",
+        "font_size": 11,
+        "bold": False,
+        "italic": False,
+        "color": BLUE_HEX,
+    },
+    "italic": {
+        "font_name": "Corbel",
+        "font_size": 11,
+        "bold": False,
+        "italic": True,
+        "color": BLACK_HEX,
+    },
+    "blue_italic": {
+        "font_name": "Corbel",
+        "font_size": 11,
+        "bold": False,
+        "italic": True,
+        "color": BLUE_HEX,
+    },
+}
