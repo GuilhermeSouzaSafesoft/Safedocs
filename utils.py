0f830fc0b4664f6ba5b7d6dd2cb6b52da1356986
GuilhermeSouzaSafesoft <guilherme.souza@safesoft.com.br>
2026-03-11 22:03:42 -0300

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,1 +1,14 @@
+def hex_to_rgb(hex_color: str):
+    hex_color = hex_color.replace("#", "").strip()
+    return RGBColor(
+        int(hex_color[0:2], 16),
+        int(hex_color[2:4], 16),
+        int(hex_color[4:6], 16)
+    )
 
+
+def set_cell_shading(cell, fill):
+    tc_pr = cell._tc.get_or_add_tcPr()
+    shd = OxmlElement("w:shd")
+    shd.set(qn("w:fill"), fill)
+    tc_pr.append(shd)
