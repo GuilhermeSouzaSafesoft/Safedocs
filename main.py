0b72270dd52e049871fea9b942c50d19a8d0a941
GuilhermeSouzaSafesoft <guilherme.souza@safesoft.com.br>
2026-03-11 22:18:42 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
 
+import json
+from config import JSON_FILE, TEMPLATE_FILE, OUTPUT_DIR, OUTPUT_FILE
+from generator import gerar_documento
+
+def main():
+    OUTPUT_DIR.mkdir(exist_ok=True)
+
+    with open(JSON_FILE, "r", encoding="utf-8") as f:
+        dados_json = json.load(f)
+
+    gerar_documento(
+        dados_json=dados_json,
+        template_file=TEMPLATE_FILE,
+        output_file=OUTPUT_FILE
+    )
+
+if __name__ == "__main__":
+    main()
